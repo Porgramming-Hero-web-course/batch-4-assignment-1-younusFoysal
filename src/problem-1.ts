978c3a519fdb3155cd811602826e6a3993fd6208
younusFoysal <younus.faysal@gmail.com>
2024-11-07 02:07:23 +0600

    problem 1 solved

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,1 +1,9 @@
-// git init
+function sumArray(numbers: number[]): number {
+    let sum: number = 0;
+    for ( let i: number = 0 ; i < numbers.length ; i++ ) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+//console.log(sumArray([1, 2, 3, 4, 5]));
